02201d83b4374fa31f5f3565690d9262eaf91bf4
lecomtemrik <lecomtemrik@gmail.com>
2023-11-11 02:30:20 +0100

    add api movies service + controller + get random movies in twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Service\ApiMoviesDatabase;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ApiMoviesDatabase $apiMoviesDatabase): Response
     {
+
+        $randomMovies = $apiMoviesDatabase->getRandomMovies("most_pop_movies");
+
+//        var_dump($randomMovies->results);
+//        dd();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'randomMovies' => $randomMovies->results,
         ]);
     }
 }
